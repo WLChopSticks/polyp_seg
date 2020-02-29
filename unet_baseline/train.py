610844c1c259d535b0362defbe9aa1ed0d36c37b
wanglei <wanglei_sy56@163.com>
2020-02-29 14:46:43 +0800

    add size loss

--- unet_baseline/train.py
+++ unet_baseline/train.py
@@ -16,7 +16,7 @@ from torchvision.transforms import RandomAffine, RandomRotation, RandomHorizonta
 from datasets_own import poly_seg, Compose_own
 from models import UNet, fcn, unet_plus
 from models.deeplab3_plus.deeplab import *
-from utils import CrossEntropyLoss2d, dice_fn, UnionLossWithCrossEntropyAndDiceLoss
+from utils import CrossEntropyLoss2d, dice_fn, UnionLossWithCrossEntropyAndDiceLoss, UnionLossWithCrossEntropyAndSize
 from datetime import datetime
 import plot.单纯测试验证集 as rstest
 
@@ -30,13 +30,13 @@ def parse_args():
     parser.add_argument('--train_csv', default=r'', type=str, help='train csv file absolute path')
     parser.add_argument('--test_csv', default=r'',  type=str, help='test csv file absolute path')
     parser.add_argument('--event_prefix', default='deeplabV3+', type=str, help='tensorboard logdir prefix')
-    parser.add_argument('--tensorboard_name', default='rectanglemask_init')
+    parser.add_argument('--tensorboard_name', default='rectanglemask_size_loss')
     parser.add_argument('--batch_size', default=2, type=int, help='batch_size')
     parser.add_argument('--gpu_order', default='0', type=str, help='gpu order')
     parser.add_argument('--torch_seed', default=2, type=int, help='torch_seed')
     parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
     parser.add_argument('--num_epoch', default=100, type=int, help='num epoch')
-    parser.add_argument('--loss', default='ce', type=str, help='ce, union')
+    parser.add_argument('--loss', default='ce+size', type=str, help='ce, union, ce+size')
     parser.add_argument('--img_size', default=(288,384), type=tuple, help='(512,512)')
     parser.add_argument('--lr_policy', default='StepLR', type=str, help='StepLR')
     parser.add_argument('--resume', default=0, type=int, help='resume from checkpoint')
@@ -173,6 +173,8 @@ def Train(train_root, train_csv, test_root, test_csv):
         criterion = CrossEntropyLoss2d().to(device)
     elif args.loss == 'union':
         criterion = UnionLossWithCrossEntropyAndDiceLoss().to(device)
+    elif args.loss == 'ce+size':
+        criterion = UnionLossWithCrossEntropyAndSize().to(device)
     else:
         print('Do not have this loss')
     optimizer = Adam(net.parameters(), lr=args.lr, amsgrad=True)
--- utils/loss.py
+++ utils/loss.py
@@ -1,8 +1,9 @@
 import torch
-from torch import nn
+from torch import nn, einsum, Tensor
 import torch.nn.functional as F
 from sklearn.metrics import jaccard_similarity_score
 import numpy as np
+from typing import cast
 
 class UnionLossWithCrossEntropyAndDiceLoss(nn.Module):
     def __init__(self, weight=None, size_average=True, ignore_index=255):
@@ -44,6 +45,51 @@ class DiceLoss(nn.Module):
         dice_coef = (2. * (input_flat * target_flat).float().sum() + smooth) / (input_flat.float().sum() + target_flat.float().sum() + smooth)
         loss = 1 - dice_coef
         return loss
+
+class UnionLossWithCrossEntropyAndSize(nn.Module):
+    def __init__(self, weight=None, size_average=True, ignore_index=255):
+        super(UnionLossWithCrossEntropyAndSize, self).__init__()
+        self.crossEntropyLoss = CrossEntropyLoss2d(weight, size_average, ignore_index)
+        self.sizeLoss = Size_Loss_naive()
+
+    def forward(self, inputs, targets):
+        loss_crossEntropy = self.crossEntropyLoss(inputs, targets)
+        loss_size = self.sizeLoss(inputs, targets)
+
+        return loss_crossEntropy + loss_size
+# ######## ------ Size loss function  (naive way) ---------- ###########
+# --- This function will push the prediction to be close ot sizeGT ---#
+class Size_Loss_naive():
+    """
+        Behaviour not exactly the same ; original numpy code used thresholding.
+        Not quite sure it will have an inmpact or not
+    """
+    def __init__(self, **kwargs):
+        # Self.idc is used to filter out some classes of the target mask. Use fancy indexing
+        super(Size_Loss_naive, self).__init__()
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
+
+    def __call__(self, inputs, targets):
+        print(inputs)
+        inputs = F.log_softmax(inputs, dim=1)
+        print(inputs)
+        assert self.simplex(inputs)
+
+        _, _, h, w = inputs.shape
+        pred_size = einsum("bcwh->bc", input)[:, 1]
+        target_size = 7845
+
+        loss = (pred_size - target_size) ** 2
+        loss = loss / (w * h)
+
+        return loss / 100
+
+    def simplex(t: Tensor, axis=1) -> bool:
+        _sum = cast(Tensor, t.sum(axis).type(torch.float32))
+        _ones = torch.ones_like(_sum, dtype=torch.float32)
+        return torch.allclose(_sum, _ones)
+
+
 
 def dice_fn(inputs, targets, threshold=0.5):
     inputs = F.softmax(inputs, dim=1)
