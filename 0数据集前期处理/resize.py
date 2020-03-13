c5b694c4c9c01833333a7b524efce4caa15142f5
wanglei <wanglei_sy56@163.com>
2020-03-13 09:17:52 +0800

    add resize code

---
+++ 0数据集前期处理/resize.py
@@ -0,0 +1,17 @@
+import os
+from PIL import Image
+import cv2
+
+out_dir = '/Users/wanglei/Documents/polyp_seg/polyp_seg/data/CVC-912/tr/images'
+gt_dir = ''
+
+for (root, dirs, files) in os.walk(out_dir):
+    for file in files:
+        image_path = os.path.join(out_dir, file)
+        image = cv2.imread(image_path)
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (384, 288))
+        cv2.imwrite(image_path, image)
+
+
+
